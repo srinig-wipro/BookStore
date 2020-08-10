22018d363ee7eadf551160acc4cf6ada325249f7
rishav2394 <dehero6@gmail.com>
2020-08-10 14:33:38 +0530

    fixed code and settings is POM.xml

--- src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
+++ src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
@@ -31,7 +31,7 @@ public class LoginFunctionalTest {
 		// new FirefoxDriver();
 				FirefoxBinary firefoxBinary = new FirefoxBinary();
         firefoxBinary.addCommandLineOptions("--headless");
-        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "/opt/firefoxdriver/geckodriver");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
         
@@ -45,7 +45,7 @@ public class LoginFunctionalTest {
 
 	@Test
 	public void loginSuccess() {
-        driver.get("http://localhost:6080/ILP_Bookstore");
+        driver.get("http://localhost:5050/ILP1dem/");
         WebElement email = driver.findElement(By.name("email"));
         WebElement pass = driver.findElement(By.name("password"));
         WebElement button = driver.findElement(By.xpath("/html/body/form/div/button"));         
@@ -57,7 +57,7 @@ public class LoginFunctionalTest {
 	
 	//@Test
 	public void loginFail() {
-        driver.get("http://localhost:6080/ILP_Bookstore");
+        driver.get("http://localhost:5050/ILP1dem/");
         WebElement email = driver.findElement(By.name("email"));
         WebElement pass = driver.findElement(By.name("password"));
         WebElement button = driver.findElement(By.xpath("/html/body/form/div/button"));         
@@ -69,7 +69,7 @@ public class LoginFunctionalTest {
 	
 	@Test
 	public void registrationSuccess() {
-        driver.get("http://localhost:6080/ILP_Bookstore/register.jsp");
+        driver.get("http://localhost:5050/ILP1dem/register.jsp");
         WebElement firstname = driver.findElement(By.name("firstname"));
         WebElement lastname = driver.findElement(By.name("lastname"));
         WebElement confirmpass = driver.findElement(By.name("confirmpass"));
@@ -87,7 +87,7 @@ public class LoginFunctionalTest {
 	
 	@Test
 	public void forgotPasswordSuccess() {
-        driver.get("http://localhost:6080/ILP_Bookstore/forgotpassword.jsp");      
+        driver.get("http://localhost:5050/ILP1dem/forgotpassword.jsp");      
         WebElement confirmpass = driver.findElement(By.name("confirmpassword"));
         WebElement email = driver.findElement(By.name("email"));
         WebElement pass = driver.findElement(By.name("newpassword"));
